ed980d7fb770ba94dcbc21a9bc238e5246b48df3
Jose Pontarolli <spontarolli@gmail.com>
2014-08-30 14:57:50 -0300

    Update stack.py

--- stack.py
+++ stack.py
@@ -9,50 +9,56 @@ import time
 import slib
 
 #print sys.argv[1]
-#imgList = glob.glob(sys.argv[1])
-imgList = sys.argv[1:]
+darklist = glob.glob(sys.argv[1])
+#imgList = sys.argv[1:]
 #print imgList
-nlist = 200 #len(imgList)
-# nlist = len(imgList)
+#nlist = 200 #len(imgList)
+nlist = len(darklist)
 median_list = nlist/2
 niter = 2
 kappa = 2
 
 #print len(imgList)
 
-ref = np.asarray(Image.open(imgList[0])).astype('uint8')
-rs,gs,bs = np.split(ref,[1,2],axis=2)
+tt = time.time()
+ref = np.asarray(Image.open(darklist[0])).astype('uint8')
+def generate_rgb(imglist):
+    rs,gs,bs = np.split(ref,[1,2],axis=2)
 
-for c, img in enumerate(imgList[1:nlist]):
-    print "\r" + str(c),
-    tmpimg = np.asarray(Image.open(img)).astype('uint8')
-    rt,gt,bt = np.split(tmpimg,[1,2],axis=2)
-    rs = np.dstack((rs,rt))
-    gs = np.dstack((gs,gt))
-    bs = np.dstack((bs,bt))
+    for c, img in enumerate(imglist[1:nlist]):
+        print "\r" + str(c+2),
+        tmpimg = np.asarray(Image.open(img)).astype('uint8')
+        rt,gt,bt = np.split(tmpimg,[1,2],axis=2)
+        rs = np.dstack((rs,rt))
+        gs = np.dstack((gs,gt))
+        bs = np.dstack((bs,bt))
+    return rs,gs,bs
 
-rf = rs.copy()
-gf = gs.copy()
-bf = bs.copy()
-
+rs,gs,bs = generate_rgb(darklist)
+#rf = rs.copy()
+#gf = gs.copy()
+#bf = bs.copy()
 print ref.shape
 
-tt = time.time()
-colors = [rs,gs,bs]
-print rs.shape
-print gs.shape
-print bs.shape
-for c,color in enumerate(colors):
-    t0 = time.time()
-    print color.shape
-    color = slib.kappasigma(color,niter,kappa).astype('uint8')
-    print color.shape
+def ksigma(rs,gs,bs):
+    colors = [rs,gs,bs]
+    print rs.shape
+    print gs.shape
+    print bs.shape
+    for c,color in enumerate(colors):
+        t0 = time.time()
+        print color.shape
+        color = slib.kappasigma(color,niter,kappa).astype('uint8')
+        print color.shape
 #    colors[c] = nditer_mean(color,[None,[0,1,-1]])
-    colors[c] = color
-    print c
-    print time.time() - t0
-final = np.dstack(colors)
-Image.fromarray(final.astype('uint8')).save('final_new.tiff')
+        colors[c] = color
+        print c
+        print time.time() - t0
+    final = np.dstack(colors)
+    return final
+
+masterdark = ksigma(rs,gs,bs)
+Image.fromarray(masterdark.astype('uint8')).save('masterdark.tiff')
 print "total time:", (time.time()-tt)
 
 
